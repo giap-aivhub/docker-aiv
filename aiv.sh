3d66da04687a08a9d3254eb6c641edebaa53ace2
Jitendra <jits023@gmail.com>
2025-05-19 17:21:40 +0100

    Fix installer

--- aiv.sh
+++ aiv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xms512m -Xmx8192m --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.util.calendar=ALL-UNNAMED --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED -Dspring.config.location=file:./config/application.yml -Dloader.path=./config/drivers -cp ./repository/econfig/:./application/aiv.jar org.springframework.boot.loader.launch.PropertiesLauncher
+java -Xms512m -Xmx8192m --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.util.calendar=ALL-UNNAMED --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED -Dspring.config.location=file:./repository/econfig/application.yml -Dloader.path=./config/drivers -cp ./repository/econfig/:./application/aiv.jar org.springframework.boot.loader.launch.PropertiesLauncher
